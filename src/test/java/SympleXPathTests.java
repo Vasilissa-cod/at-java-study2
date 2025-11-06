dcd51fa92ac4e5768e16a5dd4fc0e7c6c61ea751
Liya P. <alisapuhtel@gmail.com>
2025-11-06 22:57:18 +0300

    Update SympleXPathTests: add all test methods (testPageH1-H8)

--- src/test/java/SympleXPathTests.java
+++ src/test/java/SympleXPathTests.java
@@ -5,18 +5,62 @@ import static com.codeborne.selenide.Selenide.*;
 import org.junit.jupiter.api.Test;
 
 public class SympleXPathTests {
+
     @Test
-    void testTextSearch(){
+    void testPageH1() {
+        String relativeOrAbsoluteUrl;
+        open("https://slqamsk.github.io/tmp/xPath01.html");
+        $x("//h1").shouldHave(text("Учебная страница для XPath"));
+    }
+
+    @Test
+    void testPageH2() {
+        String relativeOrAbsoluteUrl;
+        open("https://slqamsk.github.io/tmp/xPath01.html");
+        $x("//p[@class='special-paragraph']").shouldHave(text("Этот параграф особенный - он единственный на странице с таким классом."));
+    }
+
+    @Test
+    void testPageH3() {
+        String relativeOrAbsoluteUrl;
+        open("https://slqamsk.github.io/tmp/xPath01.html");
+        $x("//p[@class='info-text']").shouldHave(text("Это первый информационный текст."));
+    }
+
+    @Test
+    void testPageH4() {
+        String relativeOrAbsoluteUrl;
+        open("https://slqamsk.github.io/tmp/xPath01.html");
+        $x("//p[@class='info-text'][2]").shouldHave(text("Это второй информационный текст."));
+    }
+
+    @Test
+    void testPageH5() {
+        String relativeOrAbsoluteUrl;
+        open("https://slqamsk.github.io/tmp/xPath01.html");
+        $x("//p[@class='info-text'][3]")
+                .shouldHave(text("Это третий информационный текст."));
+    }
+
+    @Test
+    void testPageH6() {
+        String relativeOrAbsoluteUrl;
+        open("https://stgansk.github.in/tmp/xPathE1.htmL");
+    }
+
+    @Test
+    void testPageH7() {
+        String relativeOrAbsoluteUrl;
         open("https://slqa.ru/cases/xPathSimpleForm/");
         $(withText("Москва")).shouldHave(text("250 единиц"));
     }
 
     @Test
-    void testNotUniqueClass1() {
+    void testPageH8() {
         open("https://slqa.ru/cases/xPathSimpleForm/");
-        $x("//*[contains(., 'Москва')]").shouldHave(text("250 единиц"));
-        $x("//*[contains(., 'Питер')]").shouldHave(text("180 единиц"));
-        $x("//*[starts-with(., 'Казахстан')]").shouldHave(text("площадь 2 724 902"));
+        $x("//*[contains(., 'Москва')]").shouldHave (text("250 единиц"));
+        $x("//*[contains(., 'Питер')]").shouldHave (text("180 единиц"));
+        $x("//*[starts-with(., 'Казахстан')]").shouldHave (text("площадь 2 724 902"));
     }
-}
 
+}
